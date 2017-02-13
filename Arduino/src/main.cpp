9f8df5060153da10e2951205faa021155d3fca46
Brett Mayson <brett@bmandesigns.com>
2017-02-12 23:33:51 -0600

    Testing analog joystick

--- Arduino/src/main.cpp
+++ Arduino/src/main.cpp
@@ -1,5 +1,9 @@
 #include "Arduino.h"
 
+//Constants
+const int Axis_X = 1;
+const int Axis_Y = 0;
+
 void setup()
 {
     Serial.begin(57600);
@@ -7,5 +11,12 @@ void setup()
 
 void loop()
 {
-    
+    int val_x = analogRead(Axis_X);
+    int val_y = analogRead(Axis_Y);
+
+    Serial.print("x");
+    Serial.println(val_x);
+    Serial.print("y");
+    Serial.println(val_y);
+    delay(100);
 }
