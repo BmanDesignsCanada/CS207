13530ddecd542ffb234285ddbabba17cda52a8a7
Brett Mayson <brett@bmandesigns.com>
2017-04-06 14:28:51 -0600

    Button and joystick work

--- Arduino/src/main.cpp
+++ Arduino/src/main.cpp
@@ -2,6 +2,7 @@
 
 #include <ShiftRegister.h>
 #include <Inputs.h>
+#include <RGB.h>
 
 //Constants
 const int AXIS_Z = 2;
@@ -12,9 +13,9 @@ const int AXIS_Y = 0;
 bool connected = false;
 
 ShiftRegister led(4,2,3);
-Inputs in(67,5,&led);
+Inputs in(64,5,&led);
 
-RGB status1(0,1,2);
+RGB status1(0,1,2,&led);
 
 void setup()
 {
@@ -40,21 +41,27 @@ void loop()
                 break;
             case 1: //Connect
                 connected = true;
-                status1.set('g');
+                status1.set('b');
                 break;
         }
     }
     if(connected)
     {
+        led.tick();
         int val_x = analogRead(AXIS_X);
-        int val_y = analogRead(AXIS_Y);
-        int val_z = analogRead(AXIS_Z);
-
+        //int val_y = analogRead(AXIS_Y);
+        //int val_z = analogRead(AXIS_Z);
         Serial.print("ax");
         Serial.println(val_x);
-        Serial.print("ay");
-        Serial.println(val_y);
-        Serial.print("az");
+        //Serial.print("ay");
+        //Serial.println(val_y);
+        //Serial.print("az");
+        //Serial.println(val_z);
+
+        led.write(18,0);
+        if(in.getState(77)){
+          led.write(18,1);
+        }
 
         in.tick();
 
