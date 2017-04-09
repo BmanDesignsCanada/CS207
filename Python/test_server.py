264e161f48639345d84693726169786710c500da
Brett Mayson <brett@bmandesigns.com>
2017-04-09 07:50:26 -0600

    rename socket

--- Python/test_server.py
+++ Python/test_server.py
@@ -4,11 +4,11 @@ def Main():
     host = "127.0.0.1"
     port = 5000
 
-    mySocket = socket.socket()
-    mySocket.bind((host,port))
+    s = socket.socket()
+    s.bind((host,port))
 
-    mySocket.listen(1)
-    conn, addr = mySocket.accept()
+    s.listen(1)
+    conn, addr = s.accept()
     print ("Connection from: " + str(addr))
     while True:
         data = conn.recv(1024).decode().replace("\n","")
